b1100f6874e197d754a45ad9a4c9e5edbe153197
Alex Nazarov <goglqbmo@gmail.com>
2024-06-21 14:58:45 +0300

    Added an authentication service

---
+++ EveryCupShop.Core/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using EveryCupShop.Core.Models;
+
+namespace EveryCupShop.Core.Interfaces.Services;
+
+public interface IAuthService
+{
+    Task<(string accessToken, string refreshToken)> SignIn(string email, string password);
+
+    Task<User> SignUp(string email, string password);
+
+    Task SignOut(Guid userId);
+
+    Task<(string accessToken, string refreshToken)> Refresh(string refreshToken);
+}
