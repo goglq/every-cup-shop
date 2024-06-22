50b166d540d3f94321834af09fda208907a901e3
Alex Nazarov <goglqbmo@gmail.com>
2024-06-22 23:31:08 +0300

    New entity types' configs

---
+++ EveryCupShop.Infrastructure/EntityConfigurations/CupEntityConfig.cs
@@ -0,0 +1,23 @@
+﻿using EveryCupShop.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EveryCupShop.Infrastructure.EntityConfigurations;
+
+public class CupEntityConfig : IEntityTypeConfiguration<Cup>
+{
+    public void Configure(EntityTypeBuilder<Cup> builder)
+    {
+        builder.HasKey(cup => cup.Id);
+
+        builder
+            .HasOne(cup => cup.CupShape)
+            .WithMany(shape => shape.Cups)
+            .HasForeignKey(cup => cup.CupShapeId);
+
+        builder
+            .HasOne(cup => cup.CupAttachment)
+            .WithMany(attachment => attachment.Cups)
+            .HasForeignKey(cup => cup.CupAttachmentId);
+    }
+}
