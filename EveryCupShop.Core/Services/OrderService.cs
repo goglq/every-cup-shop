eea1c846dcd6fe454edc90dc850a9de4734f1e99
Alex Nazarov <goglqbmo@gmail.com>
2024-06-22 17:31:30 +0300

    Added implementations of the new services

---
+++ EveryCupShop.Core/Services/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using EveryCupShop.Core.Interfaces.Repositories;
+using EveryCupShop.Core.Interfaces.Services;
+using EveryCupShop.Core.Models;
+
+namespace EveryCupShop.Core.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly IOrderRepository _orderRepository;
+
+    private readonly IOrderItemRepository _orderItemRepository;
+
+    public OrderService(IOrderRepository orderRepository, IOrderItemRepository orderItemRepository)
+    {
+        _orderRepository = orderRepository;
+        _orderItemRepository = orderItemRepository;
+    }
+
+    public Task<Order> CreateOrder(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}
