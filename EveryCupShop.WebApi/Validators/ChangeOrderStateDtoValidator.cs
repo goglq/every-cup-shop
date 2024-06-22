866b34f4a279e83e5199600f7c8c3b3815871812
Alex Nazarov <goglqbmo@gmail.com>
2024-06-22 23:34:49 +0300

    Added dto validators

---
+++ EveryCupShop.WebApi/Validators/ChangeOrderStateDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using EveryCupShop.Dtos;
+using FluentValidation;
+
+namespace EveryCupShop.Validators;
+
+public class ChangeOrderStateDtoValidator : AbstractValidator<ChangeOrderStateDto>
+{
+    public ChangeOrderStateDtoValidator()
+    {
+        RuleFor(dto => dto.OrderId)
+            .NotEmpty()
+            .WithMessage("Order id is not provided");
+        
+        RuleFor(dto => dto.State)
+            .NotEmpty()
+            .WithMessage("New state is not provided");
+    }
+}
