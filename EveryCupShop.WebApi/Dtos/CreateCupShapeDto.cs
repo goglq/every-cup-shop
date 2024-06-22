96826dede213e8501e9f1eb54e8be9b4e6a8ee23
Alex Nazarov <goglqbmo@gmail.com>
2024-06-23 01:07:43 +0300

    Added cup dtos

---
+++ EveryCupShop.WebApi/Dtos/CreateCupShapeDto.cs
@@ -0,0 +1,12 @@
+﻿namespace EveryCupShop.Dtos;
+
+public class CreateCupShapeDto
+{
+    public string Name { get; }
+    
+    public string Description { get; }
+    
+    public decimal Price { get; }
+    
+    public int Amount { get; }
+}
