b1e647ba3da1d9c67efaf82442f3b259ebea0b0b
Alex Nazarov <goglqbmo@gmail.com>
2024-06-22 20:43:44 +0300

    Added new repositories for the corresponding new entities

---
+++ EveryCupShop.Infrastructure/Repositories/OrderEfRepository.cs
@@ -0,0 +1,10 @@
+﻿using EveryCupShop.Core.Interfaces.Repositories;
+using EveryCupShop.Core.Models;
+using EveryCupShop.Infrastructure.Database;
+
+namespace EveryCupShop.Infrastructure.Repositories;
+
+public class OrderEfRepository : EfRepository<Order>, IOrderRepository
+{
+    public OrderEfRepository(AppDbContext context) : base(context) { }
+}
---
+++ EveryCupShop.Infrastructure/Repositories/OrderItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using EveryCupShop.Core.Interfaces.Repositories;
+using EveryCupShop.Core.Models;
+using EveryCupShop.Infrastructure.Database;
+
+namespace EveryCupShop.Infrastructure.Repositories;
+
+public class OrderItemRepository : EfRepository<OrderItem>, IOrderItemRepository
+{
+    public OrderItemRepository(AppDbContext context) : base(context) { }
+}
