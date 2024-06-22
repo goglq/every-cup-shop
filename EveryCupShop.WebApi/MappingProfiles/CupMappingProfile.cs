b11eae066cdc7769ab77f4bc38886af42197a72c
Alex Nazarov <goglqbmo@gmail.com>
2024-06-23 02:01:22 +0300

    Added cup mappings and registered them

---
+++ EveryCupShop.WebApi/MappingProfiles/CupMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EveryCupShop.Core.Models;
+using EveryCupShop.ViewModels;
+
+namespace EveryCupShop.MappingProfiles;
+
+public class CupMappingProfile : Profile
+{
+    public CupMappingProfile()
+    {
+        CreateMap<Cup, CreateCupViewModel>();
+        CreateMap<Cup, ChangeCupViewModel>();
+
+        CreateMap<CupShape, CreateCupShapeViewModel>();
+        
+        CreateMap<CupAttachment, CreateCupAttachmentViewModel>();
+    }
+}
