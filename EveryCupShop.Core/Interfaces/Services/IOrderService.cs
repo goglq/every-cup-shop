62b823236b3591ccd511ddc873ab2240303c759f
Alex Nazarov <goglqbmo@gmail.com>
2024-06-22 23:32:07 +0300

    Modified  cup and order service interfaces

--- EveryCupShop.Core/Interfaces/Services/IOrderService.cs
+++ EveryCupShop.Core/Interfaces/Services/IOrderService.cs
@@ -1,8 +1,19 @@
-﻿using EveryCupShop.Core.Models;
+﻿using EveryCupShop.Core.Enums;
+using EveryCupShop.Core.Models;
 
 namespace EveryCupShop.Core.Interfaces.Services;
 
 public interface IOrderService
 {
-    Task<Order> CreateOrder(Guid userId);
+    Task<Order> CreateOrder(Guid userId, IDictionary<Guid, int> cupIds);
+
+    Task<Order> ChangeOrderState(Guid orderId, OrderState state);
+    
+    Task<Order> DeleteOrderItem(Guid orderId);
+
+    Task<OrderItem> CreateOrderItem(Guid orderId, Guid cupId, int amount);
+
+    Task<OrderItem> SetOderItemAmount(Guid orderItemId, int amount);
+
+    Task<OrderItem> DeleteOrder(Guid orderItemId);
 }
