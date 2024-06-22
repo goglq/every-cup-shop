c9350aa2f7e70c1cd49b5bace764e19128deba6c
Alex Nazarov <goglqbmo@gmail.com>
2024-06-22 23:38:52 +0300

    Added order mappings

---
+++ EveryCupShop.WebApi/MappingProfiles/OrderMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EveryCupShop.Core.Models;
+using EveryCupShop.ViewModels;
+
+namespace EveryCupShop.MappingProfiles;
+
+public class OrderMappingProfile : Profile
+{
+    public OrderMappingProfile()
+    {
+        CreateMap<Order, CreateOrderViewModel>();
+        CreateMap<Order, ChangeOrderStateViewModel>();
+        CreateMap<Order, AddOrderItemViewModel>();
+    }
+}
