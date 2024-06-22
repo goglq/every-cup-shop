4f5d5430c8f4379fc3a63873d0551741761db276
Alex Nazarov <goglqbmo@gmail.com>
2024-06-22 23:32:07 +0300

    Modified  cup and order service interfaces

--- EveryCupShop.Core/Interfaces/Services/ICupService.cs
+++ EveryCupShop.Core/Interfaces/Services/ICupService.cs
@@ -7,4 +7,6 @@ public interface ICupService
     Task<CupAttachment> CreateAttachment(string name, string description, decimal price, int amount);
 
     Task<CupShape> CreateShape(string name, string description, decimal price, int amount);
+
+    Task<Cup> CreateCup(Guid cupShapeId, Guid cupAttachmentId);
 }
