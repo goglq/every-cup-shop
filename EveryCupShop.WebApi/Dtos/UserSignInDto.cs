992a4757a55d408c2c7a912b71746b11e0eac4eb
Alex Nazarov <goglqbmo@gmail.com>
2024-06-24 01:53:20 +0300

    Changed auth dto's type from class to record

--- EveryCupShop.WebApi/Dtos/UserSignInDto.cs
+++ EveryCupShop.WebApi/Dtos/UserSignInDto.cs
@@ -1,8 +1,3 @@
 ﻿namespace EveryCupShop.Dtos;
 
-public class UserSignInDto
-{
-    public string Email { get; set; }
-    
-    public string Password { get; set; }
-}
+public record UserSignInDto(string Email, string Password);
