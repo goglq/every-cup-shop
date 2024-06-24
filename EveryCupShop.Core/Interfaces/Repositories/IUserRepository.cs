384dd7c663eab427c62fae0d0f464a77f23f8ac4
Alex Nazarov <goglqbmo@gmail.com>
2024-06-24 06:28:15 +0300

    Added an ability to include

--- EveryCupShop.Core/Interfaces/Repositories/IUserRepository.cs
+++ EveryCupShop.Core/Interfaces/Repositories/IUserRepository.cs
@@ -1,10 +1,11 @@
-﻿using EveryCupShop.Core.Models;
+﻿using System.Linq.Expressions;
+using EveryCupShop.Core.Models;
 
 namespace EveryCupShop.Core.Interfaces.Repositories;
 
 public interface IUserRepository : IRepository<User>
 {
-    Task<User> Get(string email);
+    Task<User> Get(string email, params Expression<Func<User, object>>[] includes);
 
-    Task<User?> Find(string email);
+    Task<User?> Find(string email, params Expression<Func<User, object>>[] includes);
 }
