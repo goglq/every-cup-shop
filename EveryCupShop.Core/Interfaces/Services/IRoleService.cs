22e2034ef4745fcdf9f7cefc3519c43a4b356017
Alex Nazarov <goglqbmo@gmail.com>
2024-06-24 05:29:56 +0300

    Added a role service

---
+++ EveryCupShop.Core/Interfaces/Services/IRoleService.cs
@@ -0,0 +1,18 @@
+﻿using EveryCupShop.Core.Models;
+
+namespace EveryCupShop.Core.Interfaces.Services;
+
+public interface IRoleService
+{
+    Task<IList<Role>> GetAll();
+
+    Task<Role> Get(Guid id);
+    
+    Task<Role> Get(string name);
+    
+    Task<Role> Create(string name);
+
+    Task<Role> Put(Role role);
+
+    Task<Role> Delete(Guid id);
+}
