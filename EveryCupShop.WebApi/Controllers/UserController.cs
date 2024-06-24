3e54d2bf6ecdb7459e46a420dd788039b9fc3291
Alex Nazarov <goglqbmo@gmail.com>
2024-06-24 04:20:06 +0300

    Added update and delete methods inside the user controller

--- EveryCupShop.WebApi/Controllers/UserController.cs
+++ EveryCupShop.WebApi/Controllers/UserController.cs
@@ -37,23 +37,41 @@ public class UserController : ControllerBase
 
     [Authorize]
     [HttpGet]
-    public async Task<ActionResult<IList<User>>> GetAll()
+    public async Task<ActionResult<IList<UserViewModel>>> GetAll()
     {
-        var users = await _userService.GetUsers();
-        return Ok(users);
+        try
+        {
+            var users = await _userService.GetUsers();
+            var userViewModels = _mapper.Map<IList<UserViewModel>>(users);
+            return Ok(new ResponseMessage<IList<UserViewModel>>(userViewModels, true));
+        }
+        catch (ApiException e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(new ResponseMessage<ProblemDetails>(null, false));
+        }
     }
     
     [Authorize]
     [HttpGet("{id:guid}")]
-    public async Task<ActionResult<User>> Get(Guid id)
+    public async Task<ActionResult<UserViewModel>> Get(Guid id)
     {
-        var user = await _userService.GetUser(id);
-        return Ok(user);
+        try
+        {
+            var user = await _userService.GetUser(id);
+            var userViewModel = _mapper.Map<UserViewModel>(user);
+            return Ok(new ResponseMessage<UserViewModel>(userViewModel, true));
+        }
+        catch (ApiException e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(new ResponseMessage<ProblemDetails>(null, false));
+        }
     }
 
     [Authorize]
     [HttpPost]
-    public async Task<ActionResult<User>> Create(CreateUserDto userDto)
+    public async Task<ActionResult<CreateUserViewModel>> Create(CreateUserDto userDto)
     {
         try
         {
@@ -71,6 +89,76 @@ public class UserController : ControllerBase
         {
             _logger.LogError(e.Message);
             return BadRequest(new ResponseMessage<ProblemDetails>(null, false, e.Message));
+        }
+    }
+
+    [Authorize]
+    [HttpPatch]
+    public async Task<ActionResult> ChangeEmail(ChangeUserEmailDto dto)
+    {
+        try
+        {
+            var (id, email) = dto;
+            var updatedUser = await _userService.ChangeEmail(id, email);
+            var updatedUserViewModel = _mapper.Map<ChangeUserEmailViewModel>(updatedUser);
+            return Ok(new ResponseMessage<ChangeUserEmailViewModel>(updatedUserViewModel, true));
+        }
+        catch (ApiException e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(new ResponseMessage<ProblemDetails>(null, false));
+        }
+    }
+    
+    [Authorize]
+    [HttpPatch]
+    public async Task<ActionResult> ChangeEmail(ChangeUserPasswordDto dto)
+    {
+        try
+        {
+            var (id, password) = dto;
+            await _userService.ChangePassword(id, password);
+            return Ok(new ResponseMessage<ChangeUserPasswordViewModel>(null, true));
+        }
+        catch (ApiException e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(new ResponseMessage<ProblemDetails>(null, false));
+        }
+    }
+
+    [Authorize]
+    [HttpPut]
+    public async Task<ActionResult> ChangeUser(ChangeUserDto dto)
+    {
+        try
+        {
+            var user = _mapper.Map<User>(dto);
+            var updatedUser = await _userService.Change(user);
+            var updatedUserViewModel = _mapper.Map<UserViewModel>(updatedUser);
+            return Ok(new ResponseMessage<UserViewModel>(updatedUserViewModel, true));
+        }
+        catch (ApiException e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(new ResponseMessage<ProblemDetails>(null, false));
+        }
+    }
+
+    [Authorize]
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        try
+        {
+            var deletedUser = await _userService.Delete(id);
+            var deletedUserViewModel = _mapper.Map<UserViewModel>(deletedUser);
+            return Ok(new ResponseMessage<UserViewModel>(deletedUserViewModel, true));
+        }
+        catch (ApiException e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(new ResponseMessage<ProblemDetails>(null, false));
         }
     }
 
