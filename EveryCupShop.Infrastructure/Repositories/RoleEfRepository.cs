679e0050f47e2e189c050670dcaca8946a5619d9
Alex Nazarov <goglqbmo@gmail.com>
2024-06-24 05:29:00 +0300

    Added a role repository

---
+++ EveryCupShop.Infrastructure/Repositories/RoleEfRepository.cs
@@ -0,0 +1,15 @@
+﻿using EveryCupShop.Core.Interfaces.Repositories;
+using EveryCupShop.Core.Models;
+using EveryCupShop.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace EveryCupShop.Infrastructure.Repositories;
+
+public class RoleEfRepository : EfRepository<Role>, IRoleRepository
+{
+    public RoleEfRepository(AppDbContext context) : base(context) { }
+
+    public Task<Role> Get(string name) => Entities.FirstAsync(role => role.Name == name);
+
+    public Task<Role?> Find(string name) => Entities.FirstOrDefaultAsync(role => role.Name == name);
+}
