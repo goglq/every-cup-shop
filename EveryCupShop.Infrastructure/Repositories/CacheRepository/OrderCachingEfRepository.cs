303d661c28d6a84c6375c95d25922600d0bf1746
Alex Nazarov <goglqbmo@gmail.com>
2024-06-24 03:27:46 +0300

    Implemented caching repositories for orders and users

---
+++ EveryCupShop.Infrastructure/Repositories/CacheRepository/OrderCachingEfRepository.cs
@@ -0,0 +1,10 @@
+﻿using EveryCupShop.Core.Interfaces.Repositories;
+using EveryCupShop.Core.Models;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace EveryCupShop.Infrastructure.Repositories.CacheRepository;
+
+public class OrderCachingEfRepository : CachingEfRepository<OrderEfRepository, Order>, IOrderRepository
+{
+    public OrderCachingEfRepository(OrderEfRepository repository, IDistributedCache cache) : base(repository, cache) { }
+}
---
+++ EveryCupShop.Infrastructure/Repositories/CacheRepository/UserCachingEfRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using EveryCupShop.Core.Interfaces.Repositories;
+using EveryCupShop.Core.Models;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace EveryCupShop.Infrastructure.Repositories.CacheRepository;
+
+public class UserCachingEfRepository : CachingEfRepository<UserEfRepository, User>, IUserRepository
+{
+    public UserCachingEfRepository(UserEfRepository repository, IDistributedCache cache) : base(repository, cache)
+    {
+    }
+
+    public async Task<User> Get(string email)
+    {
+        var key = CacheKey(email);
+        var cachedItem = await FindCachedItem<User>(key);
+
+        if (cachedItem is not null)
+            return cachedItem;
+
+        var item = await Repository.Get(email);
+
+        await Cache.SetStringAsync(key, JsonSerializer.Serialize(item), Options);
+
+        return item;
+    }
+
+    public async Task<User?> Find(string email)
+    {
+        var key = CacheKey(email);
+        var cachedItem = await FindCachedItem<User>(key);
+
+        if (cachedItem is not null)
+            return cachedItem;
+
+        var item = await Repository.Find(email);
+
+        if (item is not null)
+            await Cache.SetStringAsync(key, JsonSerializer.Serialize(item), Options);
+
+        return item;
+    }
+}
