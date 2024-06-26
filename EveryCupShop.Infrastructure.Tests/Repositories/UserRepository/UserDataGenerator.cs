d06709e67b07bfb78e3753aee3278f2102fd9742
Alex Nazarov <goglqbmo@gmail.com>
2024-06-26 13:56:31 +0300

    Added an user data generator for tests

---
+++ EveryCupShop.Infrastructure.Tests/Repositories/UserRepository/UserDataGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using EveryCupShop.Core.Models;
+
+namespace EveryCupShop.Infrastructure.Tests.Repositories;
+
+public class UserDataGenerator : IEnumerable<object[]>
+{
+    private readonly IReadOnlyList<object[]> _data;
+
+    public UserDataGenerator()
+    {
+        _data = new[]
+        {
+            new object[]
+            {
+                new User
+                {
+                    Id = Guid.NewGuid(),
+                    Email = "user1@gmail.com",
+                    Password = "user1's password",
+                    Roles = new List<Role>
+                    {
+                        new()
+                        {
+                            Name = "User"
+                        }
+                    }
+                }
+            },
+            new object[]
+            {
+                new User
+                {
+                    Id = Guid.NewGuid(),
+                    Email = "user2@gmail.com",
+                    Password = "user2's password",
+                    Roles = new List<Role>
+                    {
+                        new()
+                        {
+                            Name = "User"
+                        }
+                    }
+                },
+            },
+            new object[]
+            {
+                new User
+                {
+                    Id = Guid.NewGuid(),
+                    Email = "user3@gmail.com",
+                    Password = "user3's password",
+                    Roles = new List<Role>
+                    {
+                        new()
+                        {
+                            Name = "User"
+                        }
+                    }
+                }
+            }
+        };
+    }
+
+    public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
